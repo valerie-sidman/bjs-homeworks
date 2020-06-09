7a380a6cfa74357f25a537e9ea20c5772ae4f7f0
Valerie Sidman <valerie.sidman@gmail.com>
2020-06-09 14:05:54 +0300

    Added calculation of the average rating. A welcome message has been added. Variables declared to calculate an individual formula.

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,28 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+
+	let averageRating = (algebra + geography + physics) / 3;
+
+	return averageRating;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}.`;
+
+	return message;
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+
+	let result = x * y + 5 * z + x - 1;
+
+  return result;
 }
