bda7654318895529b0a72cf4bb1ea6db17fafc39
Valerie Sidman <valerie.sidman@gmail.com>
2020-06-15 12:29:36 +0300

    Homework 1.2-extended-syntax is done.

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,44 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+
+    const discriminant = Math.pow(b, 2) - 4 * a * c;
+    let x;
+    if (discriminant < 0) {
+      x = [];
+    } else if (discriminant === 0) {
+      x = [- b / (2 * a)];
+    } else if (discriminant > 0) {
+      x = [(- b + Math.sqrt(discriminant)) / (2 * a), (- b - Math.sqrt(discriminant)) / (2 * a)];
+    }
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark;
+    if (marks.length === 0) {
+      return 0;
+    } else if (marks.length > 5) {
+      marks = marks.slice(0, 5);
+      console.log("Всего оценок больше пяти. Учитываются первые пять: " + marks);
+    }
+
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+      sum += marks[i];
+    }
+
+    averageMark = sum / marks.length;
+
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let currentDate = new Date();
+    let userAge = currentDate.getFullYear() - dateOfBirthday.getFullYear();
+
+    if (userAge >= 18) {
+      return `Не желаете ли олд-фэшн, ${name}?`
+    } else {
+      return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
+    }
 }
