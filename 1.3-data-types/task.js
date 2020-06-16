50ca6ee12a61f4e8471c360fc6a81973a5f0a7ba
Valerie Sidman <valerie.sidman@gmail.com>
2020-06-16 13:37:42 +0300

    Homework 1.3-data-types is done

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,36 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+
+    const milliseconds = 1000;
+    const seconds = 60;
+    const minutes = 60;
+    const hours = 24;
+    const days = 31;
+
+    function numberCheck(param, nameParam) {
+      param = Number(param);
+      if (Number.isNaN(param)) {
+        return `Параметр ${nameParam} содержит неправильное значение ${param}`;
+      }
+    }
+
+    numberCheck(percent, 'percent');
+    numberCheck(contribution, 'contribution');
+    numberCheck(amount, 'amount');
+    numberCheck(date, 'date');
+
+    let credit = amount - contribution;
+    let monthsOfPayment = Math.ceil((date.getTime() - new Date().getTime()) / (milliseconds * seconds * minutes * hours * days));
+    let percentageRate = (percent / 100) / 12;
+    let monthlyFee = credit * (percentageRate + percentageRate / ((Math.pow((1 + percentageRate), monthsOfPayment)) - 1));
+    let totalAmount = Number((monthsOfPayment * monthlyFee).toFixed(2));
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (!name) {
+      name = "Аноним";
+    }
+    return `Привет, мир! Меня зовут ${name}`;
 }
