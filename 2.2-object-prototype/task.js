9c6a8fe1e614122c86c20d95f25d075e05d19ac0
Valerie Sidman <valerie.sidman@gmail.com>
2020-06-19 12:03:15 +0300

    Homework 2.2-object-prototype is done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,30 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function isPalindrome() {
+  let strReverse = this.replaceAll(' ', '').toLowerCase().split('').reverse().join('');
+  if (this.replaceAll(' ', '').toLowerCase() == strReverse) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  if (marks.length === 0) {
+    return 0;
+  } else {
+    let total = 0;
+    for (i = 0; i < marks.length; i++) {
+      total += marks[i];
+    }
+    let average = total / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
+  }
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = (new Date()).getTime();
+  let birthdayDate = (new Date(birthday)).getTime();
+  let diff = now - birthdayDate;
+  let age = diff / (1000 * 60 * 60 * 24 * 365.25);
+  return age >= 18;
 }
