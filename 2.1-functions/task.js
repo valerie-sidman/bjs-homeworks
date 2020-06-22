85a368667dd87a013f4fe94ad3904f6871d3de08
Valerie Sidman <valerie.sidman@gmail.com>
2020-06-22 10:37:11 +0300

    Исправлена ошибка в первом задании

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -27,12 +27,12 @@ function showSolutionsMessage(a, b, c) {
   console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
   console.log("Значение дискриминанта: " + result.D);
 
-  if (d < 0) {
+  if (result.D < 0) {
     console.log("Уравнение не имеет вещественных корней");
-  } else if (d === 0) {
-    console.log("Уравнение имеет один корень X₁ = " + x1);
+  } else if (result.D === 0) {
+    console.log("Уравнение имеет один корень X₁ = " + result.roots[0]);
   } else {
-    console.log("Уравнение имеет два корня. X₁ = " + x1 + ", X₂ = " + x2);
+    console.log("Уравнение имеет два корня. X₁ = " + result.roots[0] + ", X₂ = " + result.roots[1]);
   }
 }
 
