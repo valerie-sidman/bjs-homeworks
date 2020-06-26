5448f64a2a0bbe9e44f2c386063ffed47cf125a5
Valerie Sidman <valerie.sidman@gmail.com>
2020-06-26 10:27:54 +0300

    Исправлена ошибка с романами, фантастикой и детективом

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -45,21 +45,21 @@ class Book extends PrintEditionItem {
 
 class NovelBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
-    super(author);
+    super(author, name, releaseDate, pagesCount);
     this.type = 'novel';
   }
 }
 
 class FantasticBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
-    super(author);
+    super(author, name, releaseDate, pagesCount);
     this.type = 'fantastic';
   }
 }
 
 class DetectiveBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
-    super(author);
+    super(author, name, releaseDate, pagesCount);
     this.type = 'detective';
   }
 }
